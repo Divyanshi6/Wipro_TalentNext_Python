252d7142c63cce41199eb26e5b6d48768e010855
Divyanshi6 <144371802+Divyanshi6@users.noreply.github.com>
2025-09-05 13:27:00 +0530

    Update classification.py

--- Regression_and_classification/classification.py
+++ Regression_and_classification/classification.py
@@ -29,22 +29,17 @@ print("Classification Report:\n", classification_report(y_test, y_pred))
 
 # Q2. Create a model that can predict that the customer has purchased item or not based on features given in the dataset. Use appropriate evaluation metrics.
 # Dataset: Social_Network_Ads.csv
-data = pd.read_csv("cancer.csv")
-data = data.drop(["id", "Unnamed: 32"], axis=1)
+data = pd.read_csv("Social_Network_Ads.csv")
 
-X = data.drop("diagnosis", axis=1)
-y = data["diagnosis"]
-
-le = LabelEncoder()
-y = le.fit_transform(y)
+X = data[["Age", "EstimatedSalary"]]
+y = data["Purchased"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = LogisticRegression(max_iter=5000)
+
+model = LogisticRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
-print("Cancer Prediction Results:")
 print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
-print("Classification Report:\n", classification_report(y_test, y_pred))
+
 
